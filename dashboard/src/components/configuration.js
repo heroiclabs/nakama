94e927be787cb0b0371f8ea8f28a431046bf421d
Chris Molozian <chris@heroiclabs.com>
2017-03-26 02:19:13 +0100

    Implement layout header with styles.

---
+++ dashboard/src/components/configuration.js
@@ -0,0 +1,5 @@
+module.exports = {
+  view: function() {
+    return "Welcome to configuration.";
+  }
+};
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,11 +2,18 @@ var m = require("mithril");
 
 var Layout = require("./components/layout");
 var HomeComponent = require("./components/home");
+var ConfComponent = require("./components/configuration");
 
-m.route(document.body, "/", {
+var root = document.body;
+m.route(root, "/", {
   "/": {
     render: function() {
       return m(Layout, m(HomeComponent));
+    }
+  },
+  "/configuration": {
+    render: function() {
+      return m(Layout, m(ConfComponent));
     }
   }
 });
