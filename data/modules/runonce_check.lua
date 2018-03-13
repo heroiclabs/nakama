c0027f3fca20ea2c08b6d592f8d3ffe7aef23423
Chris Molozian <chris@heroiclabs.com>
2018-03-13 14:20:24 +0000

    Add Lua example for run_once function.

---
+++ data/modules/runonce_check.lua
@@ -0,0 +1,29 @@
+--[[
+ Copyright 2018 The Nakama Authors
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+--]]
+
+local nk = require("nakama")
+
+--[[
+  Test run_once function calls at server startup.
+--]]
+
+nk.run_once(function(context)
+  assert(context.ExecutionMode, "run_once")
+end)
+
+nk.run_once(function(context)
+  error("Should not be executed.")
+end)
