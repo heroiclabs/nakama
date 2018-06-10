37aa528d4059522caa031eccb40286904bab6e24
Andrei Mihu <andrei@heroiclabs.com>
2018-06-10 16:18:25 +0100

    Remove unnecessary log messages.

--- server/session_ws.go
+++ server/session_ws.go
@@ -117,7 +117,6 @@ func (s *sessionWS) Consume(processRequest func(logger *zap.Logger, session Sess
 	s.conn.SetReadDeadline(time.Now().Add(time.Duration(s.config.GetSocket().PongWaitMs) * time.Millisecond))
 	s.conn.SetPongHandler(func(string) error {
 		s.conn.SetReadDeadline(time.Now().Add(time.Duration(s.config.GetSocket().PongWaitMs) * time.Millisecond))
-		s.logger.Debug("Received WebSocket PONG")
 		return nil
 	})
 
@@ -205,7 +204,6 @@ func (s *sessionWS) pingNow() bool {
 		s.cleanupClosedConnection()
 		return false
 	}
-	s.logger.Debug("Sent WebSocket PING")
 	return true
 }
 
