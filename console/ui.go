21229d1ffb64d96c4ceebb46aac35a7abbdd70d0
Mo Firouz <mo@heroiclabs.com>
2018-08-11 00:57:22 +0100

    Pack UI components and serve through console server. Implement console accounts operations.

---
+++ console/ui.go
@@ -0,0 +1,12 @@
+package console
+
+import (
+	"net/http"
+
+	"github.com/gobuffalo/packr"
+)
+
+func Handler() http.Handler {
+	uiBox := packr.NewBox("./ui/dist") // path must be string not a variable for packr to understand
+	return http.FileServer(uiBox)
+}
--- server/console_account.go
+++ server/console_account.go
@@ -16,29 +16,122 @@ package server
 
 import (
 	"context"
+	"database/sql"
 
+	"github.com/cockroachdb/cockroach-go/crdb"
+	"github.com/gofrs/uuid"
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/heroiclabs/nakama/api"
 	"github.com/heroiclabs/nakama/console"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *ConsoleServer) DeleteAccount(ctx context.Context, in *console.AccountDeleteRequest) (*empty.Empty, error) {
-	if in.RecordDeletion == nil || in.RecordDeletion.GetValue() {
-		s.DeleteAccountRecorded(ctx, in)
+	userID := uuid.FromStringOrNil(in.Id)
+	if userID == uuid.Nil {
+		return nil, status.Error(codes.InvalidArgument, "Invalid user ID was provided.")
 	}
 
-	// TODO complete account purge - refactor code above.
-	return nil, nil
+	tx, err := s.db.Begin()
+	if err != nil {
+		s.logger.Error("Could not begin database transaction.", zap.Error(err))
+		return nil, status.Error(codes.Internal, "An error occurred while trying to delete the user.")
+	}
+
+	if err := crdb.ExecuteInTx(context.Background(), tx, func() error {
+		count, err := DeleteUser(tx, userID)
+		if err != nil {
+			s.logger.Debug("Could not delete user", zap.Error(err), zap.String("user_id", in.Id))
+			return err
+		} else if count == 0 {
+			s.logger.Info("No user was found to delete. Skipping blacklist.", zap.String("user_id", in.Id))
+			return nil
+		}
+
+		err = LeaderboardRecordsDeleteAll(s.logger, tx, userID)
+		if err != nil {
+			s.logger.Debug("Could not delete leaderboard records.", zap.Error(err), zap.String("user_id", in.Id))
+			return err
+		}
+
+		err = GroupDeleteAll(s.logger, tx, userID)
+		if err != nil {
+			s.logger.Debug("Could not delete groups and relationships.", zap.Error(err), zap.String("user_id", in.Id))
+			return err
+		}
+
+		if in.RecordDeletion == nil || in.RecordDeletion.GetValue() {
+			return s.RecordAccountDeletion(tx, userID)
+		}
+
+		return nil
+	}); err != nil {
+		s.logger.Error("Error occurred while trying to delete the user.", zap.Error(err), zap.String("user_id", in.Id))
+		return nil, status.Error(codes.Internal, "An error occurred while trying to delete the user.")
+	}
+
+	return &empty.Empty{}, nil
 }
 
 func (s *ConsoleServer) DeleteAccounts(context.Context, *empty.Empty) (*empty.Empty, error) {
-	return nil, nil
+	query := "TRUNCATE TABLE users, leaderboard, groups CASCADE"
+	_, err := s.db.Exec(query)
+
+	if err != nil {
+		s.logger.Error("Error occurred while trying to deleting all users.", zap.Error(err))
+		return nil, status.Error(codes.Internal, "An error occurred while trying to delete all users.")
+	}
+
+	return &empty.Empty{}, nil
 }
 
 func (s *ConsoleServer) GetAccount(ctx context.Context, in *console.AccountIdRequest) (*api.Account, error) {
-	return nil, nil
+	userID := uuid.FromStringOrNil(in.Id)
+	if userID == uuid.Nil {
+		return nil, status.Error(codes.InvalidArgument, "Invalid user ID was provided.")
+	}
+
+	// Core user account.
+	account, err := GetAccount(s.logger, s.db, nil, userID)
+	if err != nil {
+		if err == ErrAccountNotFound {
+			return nil, status.Error(codes.NotFound, "Account not found.")
+		}
+		s.logger.Error("Could not export account data", zap.Error(err), zap.String("user_id", in.Id))
+		return nil, status.Error(codes.Internal, "An error occurred while trying to export user data.")
+	}
+
+	return account, nil
 }
 
 func (s *ConsoleServer) ListAccounts(context.Context, *empty.Empty) (*console.AccountList, error) {
-	return nil, nil
+	rows, err := s.db.Query("SELECT id FROM users WHERE id != $1 ORDER BY update_time DESC LIMIT 100", uuid.Nil)
+	if err != nil {
+		s.logger.Error("Could not list users.", zap.Error(err))
+		return nil, status.Error(codes.Internal, "An error occurred while trying to list users.")
+	}
+
+	userIDs := make([]string, 0)
+	for rows.Next() {
+		var userID sql.NullString
+		if rows.Scan(&userID); err != nil {
+			s.logger.Error("Could not list users.", zap.Error(err))
+			return nil, status.Error(codes.Internal, "An error occurred while trying to list users.")
+		}
+		userIDs = append(userIDs, userID.String)
+	}
+
+	accounts := make([]*api.Account, 0)
+	for _, id := range userIDs {
+		account, err := GetAccount(s.logger, s.db, nil, uuid.Must(uuid.FromString(id)))
+		if err != nil {
+			s.logger.Error("Could not get user while listing.", zap.Error(err))
+			return nil, status.Error(codes.Internal, "An error occurred while trying to list users.")
+		}
+		accounts = append(accounts, account)
+	}
+
+	return &console.AccountList{Accounts: accounts}, nil
 }
