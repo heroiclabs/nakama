15d800821f199415e522a8509e973bbb197f3371
Chris Molozian <chris@heroiclabs.com>
2018-09-06 01:23:22 +0100

    Improve Postgres compat.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,9 @@ The format is based on [keep a changelog](http://keepachangelog.com) and this pr
 - Lua runtime token generator function now returns a second value representing the token's expiry.
 - Add local cache for in-memory storage to the Lua runtime.
 
+### Changed
+- Improved Postgres compatibility on TIMESTAMPTZ types.
+
 ### Fixed
 - Correctly merge new friend records when importing from Facebook.
 - Log correct registered hook names at startup.
--- migrate/sql/20180103142001_initial_schema.sql
+++ migrate/sql/20180103142001_initial_schema.sql
@@ -38,8 +38,8 @@ CREATE TABLE IF NOT EXISTS users (
     edge_count    INT           DEFAULT 0 CHECK (edge_count >= 0) NOT NULL,
     create_time   TIMESTAMPTZ   DEFAULT now() NOT NULL,
     update_time   TIMESTAMPTZ   DEFAULT now() NOT NULL,
-    verify_time   TIMESTAMPTZ   DEFAULT CAST(0 AS TIMESTAMPTZ) NOT NULL,
-    disable_time  TIMESTAMPTZ   DEFAULT CAST(0 AS TIMESTAMPTZ) NOT NULL
+    verify_time   TIMESTAMPTZ   DEFAULT '1970-01-01 00:00:00' NOT NULL,
+    disable_time  TIMESTAMPTZ   DEFAULT '1970-01-01 00:00:00' NOT NULL
 );
 
 -- Setup System user.
@@ -147,7 +147,7 @@ CREATE TABLE IF NOT EXISTS leaderboard_record (
   metadata       JSONB         DEFAULT '{}' NOT NULL,
   create_time    TIMESTAMPTZ   DEFAULT now() NOT NULL,
   update_time    TIMESTAMPTZ   DEFAULT now() NOT NULL,
-  expiry_time    TIMESTAMPTZ   DEFAULT CAST(0 AS TIMESTAMPTZ) NOT NULL,
+  expiry_time    TIMESTAMPTZ   DEFAULT '1970-01-01 00:00:00' NOT NULL,
 
   UNIQUE (owner_id, leaderboard_id, expiry_time)
 );
@@ -187,7 +187,7 @@ CREATE TABLE IF NOT EXISTS groups (
   max_count    INT           DEFAULT 100 CHECK (max_count >= 1) NOT NULL,
   create_time  TIMESTAMPTZ   DEFAULT now() NOT NULL,
   update_time  TIMESTAMPTZ   DEFAULT now() NOT NULL,
-  disable_time TIMESTAMPTZ   DEFAULT CAST(0 AS TIMESTAMPTZ) NOT NULL
+  disable_time TIMESTAMPTZ   DEFAULT '1970-01-01 00:00:00' NOT NULL
 );
 CREATE INDEX IF NOT EXISTS edge_count_update_time_id_idx ON groups (disable_time, edge_count, update_time, id);
 CREATE INDEX IF NOT EXISTS update_time_edge_count_id_idx ON groups (disable_time, update_time, edge_count, id);
