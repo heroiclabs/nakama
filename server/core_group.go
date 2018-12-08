705decb85ed26272e8333162c935057674fdec7e
Mo Firouz <mo@heroiclabs.com>
2018-12-08 13:23:02 +0000

    Better error codes.

--- server/core_group.go
+++ server/core_group.go
@@ -691,15 +691,15 @@ RETURNING state`
 				query = "UPDATE groups SET edge_count = edge_count + 1, update_time = now() WHERE id = $1::UUID AND edge_count+1 <= max_count"
 				res, err := tx.ExecContext(ctx, query, groupID)
 				if err != nil {
-					logger.Debug("Could not update group edge_count.", zap.String("group_id", groupID.String()), zap.Error(err))
+					logger.Debug("Could not update group edge_count.", zap.String("group_id", groupID.String()), zap.String("user_id", uid.String()), zap.Error(err))
 					return err
 				}
 
 				if rowsAffected, err := res.RowsAffected(); err != nil {
-					logger.Debug("Could not retrieve affect rows.", zap.String("group_id", groupID.String()), zap.Error(err))
+					logger.Debug("Could not retrieve affect rows.", zap.String("group_id", groupID.String()), zap.String("user_id", uid.String()), zap.Error(err))
 					return err
 				} else if rowsAffected == 0 {
-					logger.Debug("Did not update group edge count - check edge count has not reached max count.", zap.String("group_id", groupID.String()))
+					logger.Debug("Did not update group edge count - check edge count has not reached max count.", zap.String("group_id", groupID.String()), zap.String("user_id", uid.String()))
 					return errors.New("Did not update group - check edge count has not reached max count")
 				}
 
@@ -707,7 +707,7 @@ RETURNING state`
 		}
 		return nil
 	}); err != nil {
-		logger.Error("Error promote users from group.", zap.Error(err))
+		logger.Error("Error promote users in group.", zap.Error(err))
 		return err
 	}
 
