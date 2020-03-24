6fa0567ef03937009fb8bc16a86522a8809608a0
Chris Molozian <chris@heroiclabs.com>
2020-03-24 14:48:25 +0000

    Add Twitter Bootstrap to project.

--- console/ui/src/app/app.module.ts
+++ console/ui/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
