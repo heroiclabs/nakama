df1a5dc8beb95a028984e3d581c9b8990f422bec
Mo Firouz <mo@heroiclabs.com>
2020-08-28 19:42:08 +0100

    Reimplement Nakama Console with Angular 10.

---
+++ console/ui/src/app/login/login.component.ts
@@ -0,0 +1,82 @@
+// Copyright 2020 The Nakama Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Component, Injectable, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {AuthenticationService} from '../authentication.service';
+import {SegmentService} from 'ngx-segment-analytics';
+
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  public error = '';
+  public loginForm!: FormGroup;
+  public submitted!: boolean;
+  public loading!: boolean;
+  private returnUrl!: string;
+
+  constructor(
+    private segment: SegmentService,
+    private readonly formBuilder: FormBuilder,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly authenticationService: AuthenticationService
+  ) {}
+
+  ngOnInit() {
+    this.segment.page('/login');
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+    });
+    this.returnUrl = this.route.snapshot.queryParams.next || '/';
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.error = '';
+    this.loading = false;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.authenticationService.login(this.f.username.value, this.f.password.value)
+      .subscribe(session => {
+        this.loading = true;
+        this.loginForm.reset();
+        this.submitted = false;
+        this.router.navigate([this.returnUrl]);
+      }, err => {this.error = err; this.submitted = false; });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+}
+
+@Injectable({providedIn: 'root'})
+export class LoginGuard implements CanActivate {
+  constructor(private readonly authService: AuthenticationService, private readonly router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.authService.currentSessionValue) {
+      const _ = this.router.navigate(['/']);
+      return false;
+    }
+
+    return true;
+  }
+}
