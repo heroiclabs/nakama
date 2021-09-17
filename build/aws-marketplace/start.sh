015f920d83c58d744910bd6952a9ea9c8f75447b
Michal Harish <michal.haris@heroiclabs.com>
2021-09-17 14:32:27 +0200

    added separate Setup container to the AWS Marketplace product (#675)

--- build/aws-marketplace/start.sh
+++ build/aws-marketplace/start.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
+set -e
 
 if [[ -z "$1" ]]; then
-  echo "the first argument must be a database host"
-  exit 1;
-fi
-if [[ -z "$2" ]]; then
-  echo "the second argument must be a database password for the postgres user"
-  exit 1;
-fi
-/nakama/nakama migrate up --database.address  postgres:$2@$1:5432
-if [ $? -ne 0 ]; then
+  echo "the first argument must be a database username"
   exit 1;
 fi
 
+if [[ -z "$2" ]]; then
+  echo "the second argument must be a database password"
+  exit 1;
+fi
+
+if [[ -z "$3" ]]; then
+  echo "the third argument must be a database host"
+  exit 1;
+fi
+
+if [[ "$4" == "-setup" ]]; then
+  source /setup.sh
+  exit 0;
+fi
+
+/nakama/nakama migrate up --database.address  nakama:nakama@$3:5432/nakama
 
 rm -rf /nakama-data/*
-if [ $? -ne 0 ]; then
-  exit 2;
-fi
+
 mkdir -p /nakama-data && touch /nakama-data/config.yaml
-if [ $? -ne 0 ]; then
-  exit 2;
+
+if [[ ! -z "$4" ]]; then
+  aws s3 cp s3://$4/ /nakama-data/ --recursive
 fi
 
-if [[ ! -z "$3" ]]; then
-  aws s3 cp s3://$3/ /nakama-data/ --recursive
-  if [ $? -ne 0 ]; then
-    exit 3;
-  fi
-fi
-
-/nakama/nakama --config /nakama-data/config.yaml --data_dir /nakama-data --runtime.path /nakama-data --database.address postgres:$2@$1:5432
+/nakama/nakama --config /nakama-data/config.yaml --data_dir /nakama-data --runtime.path /nakama-data --database.address nakama:nakama@$3:5432/nakama
