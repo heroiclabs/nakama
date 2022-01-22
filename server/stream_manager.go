56ab66f6a885313f404d07ca141e41bec12641c4
Andrei Mihu <andrei@heroiclabs.com>
2022-01-21 18:22:46 -0800

    Refactor and add missing license headers.

--- server/stream_manager.go
+++ server/stream_manager.go
@@ -112,17 +112,3 @@ func HashFromId(id uuid.UUID) [6]byte {
 	copy(idArr[:], id[10:])
 	return idArr
 }
-
-type testStreamManager struct{}
-
-func (t testStreamManager) UserJoin(stream PresenceStream, userID, sessionID uuid.UUID, hidden, persistence bool, status string) (bool, bool, error) {
-	return true, true, nil
-}
-
-func (t testStreamManager) UserUpdate(stream PresenceStream, userID, sessionID uuid.UUID, hidden, persistence bool, status string) (bool, error) {
-	return true, nil
-}
-
-func (t testStreamManager) UserLeave(stream PresenceStream, userID, sessionID uuid.UUID) error {
-	return nil
-}
