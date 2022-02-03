4065206b0b71b989182bc6b829314a386c73f4ff
Fernando Takagi <fer.nando36@gmail.com>
2022-02-03 17:48:28 -0300

    baseline runtimejs openapi project

---
+++ console/openapi-gen-angular/types.go
@@ -0,0 +1,53 @@
+package main
+
+type Schema struct {
+	Paths map[string]map[string]struct {
+		Summary     string
+		OperationId string
+		Responses   struct {
+			Ok struct {
+				Schema struct {
+					Ref string `json:"$ref"`
+				}
+			} `json:"200"`
+		}
+		Parameters []struct {
+			Name     string
+			In       string
+			Required bool
+			Type     string   // used with primitives
+			Items    struct { // used with type "array"
+				Type string
+			}
+			Schema struct { // used with http body
+				Type string
+				Ref  string `json:"$ref"`
+			}
+		}
+		Security []map[string][]struct{}
+	}
+	Tags []struct {
+		Name string
+	}
+	Definitions map[string]Definition
+}
+
+type Definition struct {
+	Properties map[string]struct {
+		Type  string
+		Ref   string   `json:"$ref"` // used with object
+		Items struct { // used with type "array"
+			Type string
+			Ref  string `json:"$ref"`
+		}
+		AdditionalProperties struct {
+			Type string // used with type "map"
+		}
+		Format      string // used with type "boolean"
+		Description string
+	}
+	Enum        []string
+	Description string
+	// used only by enums
+	Title string
+}
