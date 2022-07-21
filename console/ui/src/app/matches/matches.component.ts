26690ce3237a8b16cc462a65700a895dc2cc8847
Fernando Takagi <fernando@heroiclabs.com>
2022-07-21 16:57:02 -0300

    Front logic

--- console/ui/src/app/matches/matches.component.ts
+++ console/ui/src/app/matches/matches.component.ts
@@ -13,10 +13,18 @@
 // limitations under the License.
 
 import {Component, Injectable, OnInit} from '@angular/core';
-import {ActivatedRoute, ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
-import {Observable} from 'rxjs';
-import {ApiMatch, ApiMatchList, ConsoleService, MatchState, RealtimeUserPresence} from '../console.service';
-import {FormGroup} from "@angular/forms";
+import {ActivatedRoute, ActivatedRouteSnapshot, Params, Resolve, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable, of} from 'rxjs';
+import {
+  ApiChannelMessageList,
+  ApiMatch,
+  ApiMatchList,
+  ConsoleService,
+  MatchState,
+  RealtimeUserPresence
+} from '../console.service';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {catchError} from "rxjs/operators";
 
 @Component({
   templateUrl: './matches.component.html',
@@ -28,26 +36,104 @@ export class MatchesComponent implements OnInit {
   public matchStates: Array<MatchState> = [];
   public matchStatesOpen: Array<boolean> = [];
   public updated = false;
-  public searchForm: FormGroup;
+  public searchForm1: FormGroup;
+  public searchForm2: FormGroup;
+  public searchForm3: FormGroup;
+  public type: number
   public activeType = 'All';
   public readonly types = ['All', 'Authoritative', 'Relayed'];
+  public activeNode = 'All Nodes';
+  public nodes: Array<string> = ['All Nodes'];
 
   constructor(
     private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly formBuilder: FormBuilder,
     private readonly consoleService: ConsoleService,
-  ) {}
+  ) {
+    this.searchForm1 = this.formBuilder.group({
+      match_id: '',
+    });
+    this.searchForm2 = this.formBuilder.group({
+      match_id: '',
+    });
+    this.searchForm3 = this.formBuilder.group({
+      query: '',
+    });
+  }
 
   ngOnInit(): void {
+    const qp = this.route.snapshot.queryParamMap;
+
+    this.f1.match_id.setValue(qp.get('match_id'));
+    this.f2.match_id.setValue(qp.get('match_id'));
+    this.f3.query.setValue(qp.get('query'));
+
+    let qType = qp.get("type");
+    this.type = Number(qType)
+
     this.route.data.subscribe(
       d => {
-        this.matches.length = 0;
-        this.matches.push(...d[0].matches);
-        this.matchStates.length = this.matches.length;
-        this.matchStatesOpen.length = this.matches.length;
+        if (d) {
+          if (d[0]) {
+            this.error = '';
+            this.matches.length = 0;
+            this.matches.push(...d[0].matches);
+            this.matchStates.length = this.matches.length;
+            this.matchStatesOpen.length = this.matches.length;
+          }
+          if (d.error) {
+            this.error = d.error;
+          }
+        }
       },
       err => {
         this.error = err;
       });
+
+    if (qType === null) {
+      this.type = 0;
+      this.activeType = this.types[0]
+    } else {
+      if (this.type == 0 || this.type == 1 || this.type == 2) {
+        this.activeType = this.types[this.type - 2]
+      } else {
+        this.error = "Invalid type."
+      }
+    }
+  }
+
+  search() : void {
+    list(this.consoleService, this.type, this.type == 0? this.f1.match_id.value: this.f2.match_id.value, this.f3.query.value, null).subscribe(d => this.postData(d), err => { this.error = err;});
+  }
+
+  postData(d) {
+    console.log(d)
+    this.error = '';
+    this.matches.length = 0;
+    this.matches.push(...d.matches);
+    this.matchStates.length = this.matches.length;
+    this.matchStatesOpen.length = this.matches.length;
+
+    let params: Params;
+    switch(this.type) {
+      case (0):
+        params = {type: this.type, match_id: this.f1.match_id.value};
+        break;
+      case (1):
+        params = {type: this.type, match_id: this.f2.match_id.value};
+        break;
+      case (2):
+        params = {
+          type: this.type,
+          query: this.f3.query,
+        };
+        break;
+    }
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: params,
+    });
   }
 
   getMatchState(i: number, match: ApiMatch): void {
@@ -71,6 +157,16 @@ export class MatchesComponent implements OnInit {
   getMatchPresencesString(ps: Array<RealtimeUserPresence>): string {
     return JSON.stringify(ps);
   }
+
+  get f1(): any {
+    return this.searchForm1.controls;
+  }
+  get f2(): any {
+    return this.searchForm2.controls;
+  }
+  get f3(): any {
+    return this.searchForm3.controls;
+  }
 }
 
 @Injectable({providedIn: 'root'})
@@ -78,6 +174,22 @@ export class MatchesResolver implements Resolve<ApiMatchList> {
   constructor(private readonly consoleService: ConsoleService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ApiMatchList> {
-    return this.consoleService.listMatches('', null, null, null, null, null, null);
+    let type = Number(route.queryParamMap.get('type'));
+    return list(this.consoleService, type, route.queryParamMap.get('match_id'), null, route.queryParamMap.get('query')).pipe(catchError(error => {
+      route.data = {...route.data, error};
+      return of(null);
+    }));
   }
+}
+
+function list(service: ConsoleService, type: number, matchId: string, query: string, node: string) : Observable<ApiMatchList> {
+  switch(type) {
+  case (0):
+    return service.listMatches('', null, null, null, null, null, matchId);
+  case (1):
+    return service.listMatches('', null, true, null, null, null, matchId);
+  case (2):
+    return service.listMatches('', null, false, null, null, null, query);
+  }
+  return of(null)
 }
