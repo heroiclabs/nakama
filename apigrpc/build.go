32793e54ac1621b8080d876a1f48ceab68a58047
Simon Esposito <simon@heroiclabs.com>
2022-11-11 17:46:54 +0000

    Update generated swagger.
    Update grpc-gateway openapi gen opts.

--- apigrpc/build.go
+++ apigrpc/build.go
@@ -14,4 +14,4 @@
 
 package apigrpc
 
-//go:generate protoc -I. -I../vendor -I../build/grpc-gateway-v2.3.0/third_party/googleapis -I../vendor/github.com/grpc-ecosystem/grpc-gateway/v2 --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --grpc-gateway_out=. --grpc-gateway_opt=paths=source_relative --grpc-gateway_opt=logtostderr=true --openapiv2_out=. --openapiv2_opt=logtostderr=true apigrpc.proto
+//go:generate protoc -I. -I../vendor -I../build/grpc-gateway-v2.3.0/third_party/googleapis -I../vendor/github.com/grpc-ecosystem/grpc-gateway/v2 --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --grpc-gateway_out=. --grpc-gateway_opt=paths=source_relative --grpc-gateway_opt=logtostderr=true --openapiv2_out=. --openapiv2_opt=logtostderr=true,allow_delete_body=true apigrpc.proto
