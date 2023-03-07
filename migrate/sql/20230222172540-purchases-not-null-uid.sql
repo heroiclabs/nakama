4c3e3b914c460458503bd7e508971bfac481d8be
Simon Esposito <simon@heroiclabs.com>
2023-03-07 17:14:51 +0000

    Support purchases-not-null-uid.sql migration crdb.
    Fix purchases-not-null-uid.sql migration up for crdb.
    The migration failed because pg and crdb name fk constraints differently.
    Set the migration as notransaction because crdb fails to apply it if
    multiple alter table statements are made on the same fk within the
    same transaction.

--- migrate/sql/20230222172540-purchases-not-null-uid.sql
+++ migrate/sql/20230222172540-purchases-not-null-uid.sql
@@ -14,11 +14,14 @@
  * limitations under the License.
  */
 
--- +migrate Up
+-- +migrate Up notransaction
+-- https://stackoverflow.com/questions/68803747/encapsulating-a-drop-and-add-constraint-in-a-transaction
 ALTER TABLE purchase
-    DROP CONSTRAINT purchase_user_id_fkey;
+    DROP CONSTRAINT IF EXISTS purchase_user_id_fkey,
+    DROP CONSTRAINT IF EXISTS fk_user_id_ref_users;
 ALTER TABLE subscription
-    DROP CONSTRAINT subscription_user_id_fkey;
+    DROP CONSTRAINT IF EXISTS subscription_user_id_fkey,
+    DROP CONSTRAINT IF EXISTS fk_user_id_ref_users;
 
 UPDATE purchase
     SET user_id = '00000000-0000-0000-0000-000000000000' WHERE user_id IS NULL;
@@ -26,13 +29,11 @@ UPDATE subscription
     SET user_id = '00000000-0000-0000-0000-000000000000' WHERE user_id IS NULL;
 
 ALTER TABLE purchase
-    ALTER COLUMN user_id SET NOT NULL;
-ALTER TABLE purchase
+    ALTER COLUMN user_id SET NOT NULL,
     ALTER COLUMN user_id SET DEFAULT '00000000-0000-0000-0000-000000000000';
 
 ALTER TABLE subscription
-    ALTER COLUMN user_id SET NOT NULL;
-ALTER TABLE subscription
+    ALTER COLUMN user_id SET NOT NULL,
     ALTER COLUMN user_id SET DEFAULT '00000000-0000-0000-0000-000000000000';
 
 ALTER TABLE purchase
@@ -40,20 +41,21 @@ ALTER TABLE purchase
 ALTER TABLE subscription
     ADD CONSTRAINT subscription_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET DEFAULT;
 
--- +migrate Down
+-- +migrate Down notransaction
+-- https://stackoverflow.com/questions/68803747/encapsulating-a-drop-and-add-constraint-in-a-transaction
 ALTER TABLE purchase
-    DROP CONSTRAINT purchase_user_id_fkey;
+    DROP CONSTRAINT IF EXISTS purchase_user_id_fkey,
+    DROP CONSTRAINT IF EXISTS fk_user_id_ref_users;
 ALTER TABLE subscription
-    DROP CONSTRAINT subscription_user_id_fkey;
+    DROP CONSTRAINT IF EXISTS subscription_user_id_fkey,
+    DROP CONSTRAINT IF EXISTS fk_user_id_ref_users;
 
 ALTER TABLE purchase
-    ALTER COLUMN user_id DROP DEFAULT;
-ALTER TABLE purchase
+    ALTER COLUMN user_id DROP DEFAULT,
     ALTER COLUMN user_id DROP NOT NULL;
 
 ALTER TABLE subscription
-    ALTER COLUMN user_id DROP DEFAULT;
-ALTER TABLE subscription
+    ALTER COLUMN user_id DROP DEFAULT,
     ALTER COLUMN user_id DROP NOT NULL;
 
 UPDATE purchase
