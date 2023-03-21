3d06f68852df2b7a64b27587e60c6d4e6871090f
Simon Esposito <simon@heroiclabs.com>
2023-03-21 19:40:09 +0000

    Split migration in two files to avoid notransaction

--- migrate/sql/20230222172540-purchases-not-null-uid-1.sql
+++ migrate/sql/20230222172540-purchases-not-null-uid-1.sql
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
--- +migrate Up notransaction
+-- +migrate Up
+-- This migration is split in two files due to the following CRDB limitation
 -- https://stackoverflow.com/questions/68803747/encapsulating-a-drop-and-add-constraint-in-a-transaction
 ALTER TABLE purchase
     DROP CONSTRAINT IF EXISTS purchase_user_id_fkey,
@@ -28,36 +29,9 @@ UPDATE purchase
 UPDATE subscription
     SET user_id = '00000000-0000-0000-0000-000000000000' WHERE user_id IS NULL;
 
-ALTER TABLE purchase
-    ALTER COLUMN user_id SET NOT NULL,
-    ALTER COLUMN user_id SET DEFAULT '00000000-0000-0000-0000-000000000000';
-
-ALTER TABLE subscription
-    ALTER COLUMN user_id SET NOT NULL,
-    ALTER COLUMN user_id SET DEFAULT '00000000-0000-0000-0000-000000000000';
-
-ALTER TABLE purchase
-    ADD CONSTRAINT purchase_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET DEFAULT;
-ALTER TABLE subscription
-    ADD CONSTRAINT subscription_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET DEFAULT;
-
--- +migrate Down notransaction
+-- +migrate Down
+-- This migration is split in two files due to the following CRDB limitation
 -- https://stackoverflow.com/questions/68803747/encapsulating-a-drop-and-add-constraint-in-a-transaction
-ALTER TABLE purchase
-    DROP CONSTRAINT IF EXISTS purchase_user_id_fkey,
-    DROP CONSTRAINT IF EXISTS fk_user_id_ref_users;
-ALTER TABLE subscription
-    DROP CONSTRAINT IF EXISTS subscription_user_id_fkey,
-    DROP CONSTRAINT IF EXISTS fk_user_id_ref_users;
-
-ALTER TABLE purchase
-    ALTER COLUMN user_id DROP DEFAULT,
-    ALTER COLUMN user_id DROP NOT NULL;
-
-ALTER TABLE subscription
-    ALTER COLUMN user_id DROP DEFAULT,
-    ALTER COLUMN user_id DROP NOT NULL;
-
 UPDATE purchase
     SET user_id = NULL WHERE user_id = '00000000-0000-0000-0000-000000000000';
 UPDATE subscription
