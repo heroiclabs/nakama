0eaff46bd139be82810559d3fcbf5ecfb00c0502
Simon Esposito <simon@heroiclabs.com>
2023-10-09 10:49:06 +0100

    Revert js function registration

--- server/runtime_javascript.go
+++ server/runtime_javascript.go
@@ -2345,7 +2345,7 @@ func evalRuntimeModules(rp *RuntimeProviderJS, modCache *RuntimeJSModuleCache, m
 	}
 	modName := modCache.Names[0]
 
-	initializer := NewRuntimeJavascriptInitModule(logger, storageIndex, callbacks, matchHandlers, announceCallbackFn)
+	initializer := NewRuntimeJavascriptInitModule(logger, modCache.Modules[modName].Ast, storageIndex, callbacks, matchHandlers, announceCallbackFn)
 	init, err := initializer.Constructor(r)
 	if err != nil {
 		return nil, err
