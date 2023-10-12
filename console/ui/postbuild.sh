1e5c09db149347273cf9d8f548439dd0247cc1bc
Simon Esposito <simon@heroiclabs.com>
2023-10-12 18:19:49 +0100

    Comment fix

--- console/ui/postbuild.sh
+++ console/ui/postbuild.sh
@@ -2,8 +2,8 @@
 
 set -exu
 
-# Replace prepend /static to hardcoded paths for .css and .js files in generated Angular index.html files
-# Required due to the deprecation of deployUrl config in Angular
+# Prepend 'static/' to hardcoded paths for .css and .js files in generated Angular index.html files
+# Required due to the deprecation of deployUrl build config option in Angular.
 perl -pi -e 's|src="(.*?(\.js)){1}"|src="static/\1"|g' dist/prod/index.html dist/prod-nt/index.html
 perl -pi -e 's|href="(.*?(\.css)){1}"|href="static/\1"|g' dist/prod/index.html dist/prod-nt/index.html
 
