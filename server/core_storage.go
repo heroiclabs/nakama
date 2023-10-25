a5cbeba33492ac3c009979537280d3a7e5512ebf
Fernando Takagi <fernando@heroiclabs.com>
2023-10-25 11:22:38 -0300

    Update core_storage.go

--- server/core_storage.go
+++ server/core_storage.go
@@ -699,11 +699,11 @@ func storagePrepBatch(batch *pgx.Batch, authoritativeWrite bool, op *StorageOpWr
 		query = `
 		INSERT INTO storage (collection, key, user_id, value, version, read, write, create_time, update_time)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, now(), now())
-		ON CONFLICT (collection, key, user_id) DO NOTHING 
+		ON CONFLICT (collection, key, user_id) DO NOTHING
 		RETURNING read, write, version, create_time, update_time`
 
 		// Outcomes:
-		// - NoRows - insert failed due to constraint violation (concurrent insert)
+		// - NoRows - insert was ignored due to constraint violation (concurrent insert or non-OCC existing row)
 	}
 
 	batch.Queue(query, params...)
