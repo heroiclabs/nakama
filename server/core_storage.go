02f6bd6b24bc7b7d93702157f52feed155a38810
Simon Esposito <simon@heroiclabs.com>
2024-01-29 12:13:41 +0000

    Remove order by from storage write queries.
    Make queries mutually exclusive using 'exists'.

--- server/core_storage.go
+++ server/core_storage.go
@@ -656,10 +656,9 @@ func storagePrepBatch(batch *pgx.Batch, authoritativeWrite bool, op *StorageOpWr
 		` + writeCheck + `
 			RETURNING read, write, version, create_time, update_time
 		)
-		(SELECT read, write, version, create_time, update_time, true AS update FROM upd)
+		(SELECT read, write, version, create_time, update_time, true AS update FROM upd WHERE EXISTS (SELECT 1 FROM upd))
 		UNION ALL
-		(SELECT read, write, version, create_time, update_time, false AS update FROM storage WHERE collection = $1 and key = $2 and user_id = $3)
-		ORDER BY update DESC
+		(SELECT read, write, version, create_time, update_time, false AS update FROM storage WHERE collection = $1 and key = $2 and user_id = $3 AND NOT EXISTS (SELECT 1 FROM upd))
 		LIMIT 1`
 
 		params = append(params, object.Version)
@@ -685,10 +684,9 @@ func storagePrepBatch(batch *pgx.Batch, authoritativeWrite bool, op *StorageOpWr
 				WHERE TRUE` + writeCheck + `
 			RETURNING read, write, version, create_time, update_time
 		)
-		(SELECT read, write, version, create_time, update_time, true AS upsert FROM upd)
+		(SELECT read, write, version, create_time, update_time, true AS upsert FROM upd WHERE EXISTS (SELECT 1 FROM upd))
 		UNION ALL
-		(SELECT read, write, version, create_time, update_time, false AS upsert FROM storage WHERE collection = $1 and key = $2 and user_id = $3)
-		ORDER BY upsert DESC
+		(SELECT read, write, version, create_time, update_time, false AS upsert FROM storage WHERE collection = $1 and key = $2 and user_id = $3 AND NOT EXISTS (SELECT 1 FROM upd))
 		LIMIT 1`
 
 		// Outcomes:
