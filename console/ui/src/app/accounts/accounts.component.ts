17bc16669ac9eaff5ca4c3db36c02f2285aabb95
Simon Esposito <simon@heroiclabs.com>
2024-05-28 11:17:35 +0100

    Allow to cancel console accounts long running queries (#1214)

--- console/ui/src/app/accounts/accounts.component.ts
+++ console/ui/src/app/accounts/accounts.component.ts
@@ -12,21 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {Component, Injectable, OnInit} from '@angular/core';
+import {Component, Injectable, OnInit, OnDestroy} from '@angular/core';
 import {ActivatedRoute, ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
 import {AccountList, ApiUser, ConsoleService, UserRole} from '../console.service';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import {UntypedFormBuilder, UntypedFormGroup} from '@angular/forms';
 import {AuthenticationService} from '../authentication.service';
-import {DeleteConfirmDialogComponent} from '../shared/delete-confirm-dialog/delete-confirm-dialog.component';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {DeleteConfirmService} from '../shared/delete-confirm.service';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   templateUrl: './accounts.component.html',
   styleUrls: ['./accounts.component.scss']
 })
-export class AccountListComponent implements OnInit {
+export class AccountListComponent implements OnInit, OnDestroy {
   public readonly systemUserId = '00000000-0000-0000-0000-000000000000';
   public error = '';
   public accountsCount = 0;
@@ -34,6 +33,8 @@ export class AccountListComponent implements OnInit {
   public nextCursor = '';
   public prevCursor = '';
   public searchForm: UntypedFormGroup;
+  public querySubject: Subject<void>;
+  public ongoingQuery = false;
 
   constructor(
     private readonly route: ActivatedRoute,
@@ -45,6 +46,7 @@ export class AccountListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.querySubject = new Subject<void>();
     this.searchForm = this.formBuilder.group({
       filter: [''],
       filter_type: [0], // 0 for all, 1 for tombstones
@@ -76,7 +78,17 @@ export class AccountListComponent implements OnInit {
       });
   }
 
+  ngOnDestroy(): void {
+    this.querySubject.next();
+    this.querySubject.complete();
+  }
+
   search(state: number): void {
+    if (this.ongoingQuery) {
+      this.querySubject.next();
+    }
+    this.ongoingQuery = true;
+
     let cursor = '';
     switch (state) {
       case -1:
@@ -91,7 +103,10 @@ export class AccountListComponent implements OnInit {
     }
 
     const tombstones = this.f.filter_type.value && this.f.filter_type.value === 1;
-    this.consoleService.listAccounts('', this.f.filter.value, tombstones, cursor).subscribe(d => {
+
+    this.consoleService.listAccounts('', this.f.filter.value, tombstones, cursor)
+      .pipe(takeUntil(this.querySubject))
+      .subscribe(d => {
       this.error = '';
 
       this.accounts.length = 0;
@@ -108,9 +123,16 @@ export class AccountListComponent implements OnInit {
         },
         queryParamsHandling: 'merge',
       });
+      this.ongoingQuery = false;
     }, err => {
       this.error = err;
+      this.ongoingQuery = false;
     });
+  }
+
+  cancelQuery(): void {
+    this.querySubject.next();
+    this.ongoingQuery = false;
   }
 
   deleteAccount(event, i: number, o: ApiUser): void {
