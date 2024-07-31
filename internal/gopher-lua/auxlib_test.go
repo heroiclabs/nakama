2a3ffcfbfe2b87a4df9efe28c1411c2a8ee03f91
Carlos Cobo <699969+toqueteos@users.noreply.github.com>
2024-07-31 17:15:06 +0200

    Remove usages of io/ioutil
    Package deprecated since Go 1.16

--- internal/gopher-lua/auxlib_test.go
+++ internal/gopher-lua/auxlib_test.go
@@ -1,7 +1,6 @@
 package lua
 
 import (
-	"io/ioutil"
 	"os"
 	"testing"
 )
@@ -298,10 +297,10 @@ func TestOptChannel(t *testing.T) {
 }
 
 func TestLoadFileForShebang(t *testing.T) {
-	tmpFile, err := ioutil.TempFile("", "")
+	tmpFile, err := os.CreateTemp("", "")
 	errorIfNotNil(t, err)
 
-	err = ioutil.WriteFile(tmpFile.Name(), []byte(`#!/path/to/lua
+	err = os.WriteFile(tmpFile.Name(), []byte(`#!/path/to/lua
 print("hello")
 `), 0644)
 	errorIfNotNil(t, err)
@@ -319,7 +318,7 @@ print("hello")
 }
 
 func TestLoadFileForEmptyFile(t *testing.T) {
-	tmpFile, err := ioutil.TempFile("", "")
+	tmpFile, err := os.CreateTemp("", "")
 	errorIfNotNil(t, err)
 
 	defer func() {
