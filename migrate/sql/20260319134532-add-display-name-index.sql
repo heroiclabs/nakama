67c17e80c6db08fd27282ce9c9b7e1a94f8a7693
Andrei Mihu <andrei@heroiclabs.com>
2026-03-20 13:46:33 +0000

    Better searching by account display name in the Console UI. (#2479)

---
+++ migrate/sql/20260319134532-add-display-name-index.sql
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2026 The Nakama Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+-- +migrate Up
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+COMMIT;
+-- Concurrent index creation cannot be done inside a transaction.
+CREATE INDEX CONCURRENTLY IF NOT EXISTS users_display_name_idx ON users USING GIN (display_name gin_trgm_ops);
+BEGIN;
+
+-- +migrate Down
+DROP INDEX IF EXISTS users_display_name_idx;
